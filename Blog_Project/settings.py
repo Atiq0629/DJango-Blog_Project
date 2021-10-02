52335700dc058cbd93a29dc746ea6402b31d688e
Atiq0629 <cross.016x@gmail.com>
2021-10-02 21:54:43 +0600

    commit for Heroku Deploy

--- Blog_Project/settings.py
+++ Blog_Project/settings.py
@@ -126,14 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = "app-root/repo/wsgi/static"
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (BASE_DIR / 'media')
-
-
 
 
 
